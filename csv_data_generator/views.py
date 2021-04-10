371ff9dc6fa7bc94e18a5fed60dcea050cef5fb3
Alymbekov <alymbekovdastan1@gmail.com>
2021-04-10 21:18:58 +0600

    add login view and templates

--- csv_data_generator/views.py
+++ csv_data_generator/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
 
-# Create your views here.
+
+class IndexView(LoginRequiredMixin, generic.TemplateView):
+    template_name = 'index.html'
--- user/views.py
+++ user/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
 
-# Create your views here.
+
+class CustomLoginView(LoginView):
+    template_name = 'login-form.html'
+
