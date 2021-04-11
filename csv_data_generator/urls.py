0b372229a06e852fe97b097304c4803922b47543
Alymbekov <alymbekovdastan1@gmail.com>
2021-04-11 14:55:19 +0600

    add schemas page style and add dynamic field logic

--- csv_data_generator/urls.py
+++ csv_data_generator/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('newschema/', views.NewSchemaView.as_view(), name='newschema'),
 ]
--- csv_data_generator/views.py
+++ csv_data_generator/views.py
@@ -1,6 +1,22 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render
 from django.views import generic
+
+User = get_user_model()
 
 
 class IndexView(LoginRequiredMixin, generic.TemplateView):
     template_name = 'index.html'
+
+
+class NewSchemaView(generic.View):
+    template_name = 'newschema.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {})
+
+    def post(self, request, *args, **kwargs):
+        result = request.POST
+        print(result)
+        return render(request, self.template_name, {})
